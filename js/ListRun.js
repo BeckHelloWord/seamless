817cb8db58df4db436a66fd9fd17a2fd77ce13d8
yuwei <shinyhz50817>
2016-06-02 18:28:58 +0800

    优化

--- js/ListRun.js
+++ js/ListRun.js
@@ -3,26 +3,35 @@
  */
 (function ($) {
     var defaults = {
-        num: 5
+        num: 5,
+        count:7
     };
     $.fn.doFun = function (options) {
         var options = $.extend(defaults, options);
 
+        /*
+        * 基本配置
+        * 获得内容个数、单个内容宽度、设置显示区域的总宽度和默认偏移
+        */
         var _this = $(this);
-        var nextChild=_this.children().slice(0,5);
-        var prevChild=_this.children().slice(-5);
-        _this.append(nextChild);
-        _this.prepend(prevChild);
         var liLength = _this.children().length;
+        insert();
         var liWidth = _this.find('div').outerWidth(true);
         _this.width(liWidth * liLength).css('left', -liWidth * options.num);
         var ulLeft = parseInt(_this.css('left'));
 
+        function insert(num){
+            var str="";
+            for(var i=0;i<options.num;i++){
+                str+=_this.children().eq(i).html();
+            }
+            _this.append(str);
+        }
+
+
         $('.next').on('click', function () {
             if (!_this.is(':animated')) {
                 ulLeft -= liWidth;
-                console.log(liLength);
-                console.log((liLength - options.num) * liWidth * -1)
                 _this.animate({'left': ulLeft}, 500, function () {
                     if (ulLeft === (liLength - options.num) * liWidth * -1) {
                         ulLeft = -liWidth * options.num;
@@ -33,14 +42,12 @@
 
         })
 
-        $('.pev').on('click', function () {
+        $('.prev').on('click', function () {
             if (!_this.is(':animated')) {
-                ulLeft -= liWidth;
-                console.log(liLength);
-                console.log((liLength - options.num) * liWidth * -1)
+                ulLeft += liWidth;
                 _this.animate({'left': ulLeft}, 500, function () {
-                    if (ulLeft === (liLength - options.num) * liWidth * -1) {
-                        ulLeft = -liWidth * options.num;
+                    if (ulLeft === 0) {
+                        ulLeft = -liWidth * options.count;
                         _this.css('left', ulLeft)
                     }
                 });
